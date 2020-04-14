647c18adce5d71dc539f6222ccfe391ab39e403a
wattoboy <57374457+wattoboy@users.noreply.github.com>
2020-04-14 13:03:00 -0700

    Added all files on repl portfolio.

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Larry Guo, Period 6]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Last_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Last_Projects.html">Projects.html</a></li></br>
+      <li><a href="Last_Resume.html">Resume.html</a></li></br>
+      <li><a href="Last_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
